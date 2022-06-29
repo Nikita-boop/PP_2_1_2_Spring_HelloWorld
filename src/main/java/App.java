d3f234461cbed6b06ae3135d9e83e861066dd05f
Nikita-boop <nikita.nebolsin@gmail.com>
2022-06-29 11:52:47 +0300

    Second Try

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.print("Compare two items of Hello World: ");
+        System.out.println(bean == bean2);
+
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(firstCat.getMessage());
+        System.out.print("Compare two items of Cat: ");
+        System.out.println(firstCat == secondCat);
     }
 }
